3036142b35e77e9cfb8c552d14d337c8bc713865
胡胜军 <https://github.com/1282901262/Hu_Test>
2019-09-24 22:47:55 +0800

    test

---
+++ src/GitHubTest.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class GitHubTest {
+    public static void main(String[] args) {
+        //	SpringApplication.run(TestSpringBootApplication.class, args);
+		/*int i = 1;
+		boolean b = !(i++ == 3) ^ (i++ ==2) && (i++==3);
+		System.out.println(b);
+		System.out.println(i);*/
+        Scanner input = new Scanner(System.in);
+        int[] shu = new int[5];
+        System.out.print("输入数字：");
+        for(int i = 0; i < shu.length; i++) {
+            int n = input.nextInt();
+            shu[i] = n;
+        }
+        System.out.println("显示数组：");
+        for(int i = 0; i < shu.length; i++) {
+            System.out.print(shu[i] + " ");
+        }
+
+        for(int i = 0; i < shu.length; i++) {
+            for(int j = 0; j<shu.length - 1 - i; j++) {
+                if(shu[j] > shu[j+1]) {
+                    int num = shu[j];
+                    shu[j] = shu[j+1];
+                    shu[j+1] = num;
+                }
+            }
+        }
+        System.out.println("显示排序后数组：");
+        for(int i = 0; i < shu.length; i++) {
+            System.out.print(shu[i] + " ");
+        }
+
+    }
+}
